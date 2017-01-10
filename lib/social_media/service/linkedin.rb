9d8af1aa88573237c0112096a7fcd055bcf5c138
Brijesh Shah <brijeshror@gmail.com>
2017-01-10 15:36:29 +0530

    Integrate adapter for linkedin service

--- lib/social_media/service/linkedin.rb
+++ lib/social_media/service/linkedin.rb
@@ -26,20 +26,44 @@ module SocialMedia::Service
 
     private
 
+    def cast_error error
+      return Error::Unauthorized if error.is_a?(Faraday::ClientError) && (error.response[:status] == 401)
+    end
+
+    def get_api_base_url
+      'https://api.linkedin.com/v1'
+    end
+
     def client
-      raise 'not implemented, yet'
+      headers = {
+        content_type: 'application/json',
+        authorization: "Bearer #{connection_params[:access_token]}"
+      }
+
+      Faraday.new(url: get_api_base_url, headers: headers) do |faraday|
+        faraday.request  :url_encoded
+        faraday.response :raise_error
+        faraday.adapter  Faraday.default_adapter
+      end
     end
 
     def send_text_message message, options
-      handle_error do
-        raise_not_provided_error
+      body = { comment: message, visibility: { code: 'anyone' } }.merge(options)
+      responce = client.post 'people/~/shares?format=json' do |req|
+        req.body = body.to_json
       end
+      (::JSON.parse responce.body rescue {})['updateKey']
     end
 
+    # :submitted_url - an url submitted with image
     def send_multipart_message message, options
-      handle_error do
-        raise_not_provided_error
-      end
+      raise_not_provided_error if options.delete(:filenames)
+      media_url = options.delete(:filename)
+
+      content = {
+        content: { submitted_image_url: media_url, submitted_url: options.delete(:submitted_url) || media_url }
+      }
+      send_text_message message, options.merge(content)
     end
   end
 end
